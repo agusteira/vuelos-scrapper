2c38e83b37bb6bcbaff0eaa1397cec910185a90e
Agustin Teira <124114633+agusteira@users.noreply.github.com>
2025-06-01 22:42:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,20 +7,18 @@ Vuelos.crear_tabla()
 
 # Insertar datos
 
-while(True):
-    ahora = datetime.now(ZoneInfo("America/Argentina/Buenos_Aires"))
-    formateado = ahora.strftime("%Y-%m-%dT%H:%M:%S")
-    vuelo_obj = Vuelos(DateTime= ahora,
-                                      FechaSalida="2025-07-01",
-                                      HoraSalida="08:00",
-                                      HoraLlegada="14:00",
-                                      LugarSalida="Buenos Aires",
-                                      LugarDestino="Miami",
-                                      Precio1=105000.00,
-                                      Precio2=125000.00)
+ahora = datetime.now(ZoneInfo("America/Argentina/Buenos_Aires"))
+formateado = ahora.strftime("%Y-%m-%dT%H:%M:%S")
+vuelo_obj = Vuelos(DateTime= ahora,
+                                  FechaSalida="2025-07-01",
+                                  HoraSalida="08:00",
+                                  HoraLlegada="14:00",
+                                  LugarSalida="Buenos Aires",
+                                  LugarDestino="Miami",
+                                  Precio1=105000.00,
+                                  Precio2=125000.00)
 
 
 # Consultar
-    vuelo_obj.crear()
-    Vuelos.leer_todo()
-    time.sleep(100)
+vuelo_obj.crear()
+Vuelos.leer_todo()
